83e2cc8d33c2a3a27d5712126b0375f2567c9037
ninjaga-coin <ninjaga.njg@gmail.com>
2025-05-31 23:20:12 +0300

    Add files via upload

--- pages/api/live-trade.js
+++ pages/api/live-trade.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      "https://public-api.birdeye.so/public/txs/token/DeezVjE84ENhzLbxNzdH5zPQnVgwk4Q93FpT9ynDpDqU?limit=1",
+      "https://public-api.birdeye.so/public/txs/token/DwSfpmu1ovgcMocdib4y1v7NXnyf4VDEVR81H8Ynpump?limit=1",
       {
         headers: {
           "X-API-KEY": "38991d74b5554611bfa5d4aa3cdfc20d", // Replace with your actual Birdeye API key
